375b8fdbd0516fee256299ebb65c3cf4ec5b7686
Michael Spivack <michael.paul.spivack@gmail.com>
2013-07-10 13:58:34 -0400

    via master learning more about merge conflicts

--- application/libraries/ConnectFour.php
+++ application/libraries/ConnectFour.php
@@ -17,6 +17,7 @@
 		
 		public function ai_player_move($game_array, $active_players_move)
 		{
+			// la lalllallalllalalalla alvl  a a alala al a la
 			$this->game_array = $game_array;			
 			$this->game_array = $this->calculate_ai_move($active_players_move);
 			return $this->game_array;	
